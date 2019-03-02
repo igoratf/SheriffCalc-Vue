35e715b53c0af0c796ffad04183c0e0592e82c93
igoratf <igor.atf@gmail.com>
2019-03-02 00:09:35 -0300

    remove unused components

--- sheriff-calculator/src/App.vue
+++ sheriff-calculator/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <Toolbar />
-    
+
     <v-container grid-list-md text-ms-center fluid>
       <router-view />
     </v-container>
@@ -12,10 +12,6 @@
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-import Baseline from "./views/Baseline";
-import Dashboard from "./views/Dashboard";
-import Player from "./components/Player";
 import Toolbar from "./components/Toolbar";
 import AddButton from "./components/AddButton";
 
--- sheriff-calculator/src/router.js
+++ sheriff-calculator/src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
 import PlayerForm from './components/PlayerForm';
 import Dashboard from "./views/Dashboard";
 
@@ -10,14 +9,6 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    },
     {
       path: '/dashboard',
       name: "dashboard",
