03039184a99db3bb6ca821d358eb259956a9f983
igoratf <igor.atf@gmail.com>
2019-03-06 18:24:52 -0300

    add max number for resources

---
+++ sheriff-calculator/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+<v-container justify-center>
+   <div class="home">
+      <h1>Sheriff of Nottingham</h1>
+      <!-- <img src="../assets/sheriff-logo.jpeg"> -->
+   </div>
+</v-container>
+</template>
+
+<script>
+export default {
+   name: "Home"
+}
+</script>
+
+<style scoped>
+.home {
+   /* font-family: 'Shojumaru', cursive; */
+   /* font-family: 'Chewy', cursive; */
+   /* font-family: 'Luckiest Guy', cursive; */
+   font-family: 'Chelsea Market', cursive;
+   /* font-family: 'Knewave', cursive; */
+   font-size: 1.5em;
+   font-weight: bold;
+}
+</style>
+
+
