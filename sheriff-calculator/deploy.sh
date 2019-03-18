be62248babddbba417c822ea1d6ed1e4ec81588c
igoratf <igor.atf@gmail.com>
2019-03-18 19:22:53 -0300

    add deploy script

--- sheriff-calculator/deploy.sh
+++ sheriff-calculator/deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f igoratf@github.com:igoratf/USERNAME.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:igoratf/SheriffCalc-Vue.git master:gh-pages
+git push -f igoratf@github.com:igoratf/SheriffCalc-Vue.git master:gh-pages
 
 cd -
